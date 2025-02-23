d370e87082909dc4f94588eab39c255fadaa1aa0
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2025-02-22 21:59:05 -0500

    Fix MCU Subsystem TB compile

--- mcu_subsys/tb/mcu_subsys_tb.sv
+++ mcu_subsys/tb/mcu_subsys_tb.sv
@@ -1,28 +1,32 @@
 module mcu_subsys_tb;
 
-    logic i_sys_clk;
-    logic i_rst_n;
+    logic sys_clk;
+    logic rst_n;
 
-    mcu_subsys_tb u_mcu_subsys_tb (
-        .i_sys_clk(i_sys_clk),
-        .i_rst_n(i_rst_n),
+    mcu_subsys_top u_mcu_subsys_top (
+        .sys_clk(sys_clk),
+        .rst_n(rst_n),
+        .periph_mem_valid(),
         .periph_mem_ready(1'b0),
+        .periph_mem_addr(),
+        .periph_mem_wdata(),
+        .periph_mem_wstrb(),
         .periph_mem_rdata(32'h0)
     );
 
     initial begin
-        i_rst_n = 1'b0;
+        rst_n = 1'b0;
         #100;
-        i_rst_n = 1'b1;
+        rst_n = 1'b1;
         #1000;
         $finish;
     end
 
     initial begin
-        i_sys_clk = 1'b0;
+        sys_clk = 1'b0;
         forever begin
             #10;
-            i_sys_clk = ~i_sys_clk;
+            sys_clk = ~sys_clk;
         end
     end
 
