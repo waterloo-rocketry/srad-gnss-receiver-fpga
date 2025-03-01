0265236d68601fb00db2e4c01b6429d2956d1df1
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2025-03-01 00:02:47 -0500

    Make SRAM Larger

--- mcu_subsys/rtl/mcu_subsys_sram.sv
+++ mcu_subsys/rtl/mcu_subsys_sram.sv
@@ -8,21 +8,21 @@ module mcu_subsys_sram (
     input logic [3:0]   mem_wstrb,
     output logic [31:0] mem_rdata);
 
-    logic [255:0] [31:0] mem;
+    logic [31:0] mem [16384-1:0];
 
     always_ff @(posedge clk or negedge rst_n) begin
         if(rst_n == 1'b0) begin
             mem_ready <= 1'b0;
             mem_rdata <= 32'h0;
         end else begin
-            mem_rdata <= mem[mem_addr[9:2]];
+            mem_rdata <= mem[mem_addr[15:2]];
 
             if(mem_valid) begin
                 mem_ready <= 1'b1;
-                if(mem_wstrb[0]) mem[mem_addr[9:2]][7:0] <= mem_wdata[7:0];
-                if(mem_wstrb[1]) mem[mem_addr[9:2]][15:8] <= mem_wdata[15:8];
-                if(mem_wstrb[2]) mem[mem_addr[9:2]][23:16] <= mem_wdata[23:16];
-                if(mem_wstrb[3]) mem[mem_addr[9:2]][31:24] <= mem_wdata[31:24];
+                if(mem_wstrb[0]) mem[mem_addr[15:2]][7:0] <= mem_wdata[7:0];
+                if(mem_wstrb[1]) mem[mem_addr[15:2]][15:8] <= mem_wdata[15:8];
+                if(mem_wstrb[2]) mem[mem_addr[15:2]][23:16] <= mem_wdata[23:16];
+                if(mem_wstrb[3]) mem[mem_addr[15:2]][31:24] <= mem_wdata[31:24];
             end else begin
                 mem_ready <= 1'b0;
             end
